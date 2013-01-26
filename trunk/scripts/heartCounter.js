344717e3f768c589167fcd2e12bb448de26db95e
matias.iacono@gmail.com <matias.iacono@gmail.com@8c771076-829a-22e6-4b9f-31edede5cb29>
2013-01-26 02:40:00 +0000

    Heart counter with void counter

--- trunk/scripts/heartCounter.js
+++ trunk/scripts/heartCounter.js
@@ -1,10 +1,15 @@
 function HeartCounter() {
 	var self = this;
+	var heartSize = 30;
+	var heart = new Image();
+	var voidHeart = new Image();
 	
-	var sprite = new Image();
-	sprite.src = "graphics/heart.png";
+	voidHeart.src = "graphics/voidheart.png";
+	heart.src = "graphics/heart.png";
 	
 	this.totalHeart = 3;
+	this.totalLives = 2;
+	
 	this.zOrder = 1000;
 	
 	this.init = function() {
@@ -16,8 +21,11 @@ function HeartCounter() {
 	}
 	
 	this.draw = function(spriteBatch) {
+		var h = heart;
+		
 		for (var i = 0; i < self.totalHeart; i++) {
-			spriteBatch.drawImage(sprite, 128 * i, 10);
+			if (i >= self.totalLives) { h = voidHeart; }
+			spriteBatch.drawImage(h, heartSize * i, 100, heartSize, heartSize);
 		}
 	}
 }
