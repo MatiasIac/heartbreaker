373718ed2bebddcbca3fa06815b47c7a8702088d
matias.iacono@gmail.com <matias.iacono@gmail.com@8c771076-829a-22e6-4b9f-31edede5cb29>
2013-01-26 00:56:58 +0000

    Framework

---
+++ scripts/BaseClass.js
@@ -0,0 +1,21 @@
+function BaseClass() {
+	var self = this;
+	
+	var sprite = new Image();
+	sprite.src = "...";
+
+	this.zOrder = 100;
+	
+	this.init = function() {
+	}
+	
+	this.visible = true;
+	
+	this.update = function(delta) {
+
+	}
+	
+	this.draw = function(context) {
+	}
+
+}
