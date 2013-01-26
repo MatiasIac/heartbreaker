5258c3b01b61a100918cfa55fa4e68b84be6617f
matias.iacono@gmail.com <matias.iacono@gmail.com@8c771076-829a-22e6-4b9f-31edede5cb29>
2013-01-26 03:12:35 +0000

    Beating

--- scripts/heartCounter.js
+++ scripts/heartCounter.js
@@ -4,20 +4,36 @@ function HeartCounter() {
 	var heart = new Image();
 	var voidHeart = new Image();
 	
+	var counter = 0;
+	var beater = function () {};
+	var beaterCounter = 0;
+	var beatSwitch = 0;
+	var beatSize = -5;
+
 	voidHeart.src = "graphics/voidheart.png";
 	heart.src = "graphics/heart.png";
 	
 	this.totalHeart = 3;
 	this.totalLives = 2;
-	
 	this.zOrder = 1000;
-	
-	this.init = function() {
-	}
-	
+	this.init = function() {}
 	this.visible = true;
 	
 	this.update = function(delta) {
+		counter += delta;
+		
+		if (counter >= 1) {
+			counter = 0;
+			beater = function () {
+				beaterCounter += delta;
+				if (beaterCounter >= 0.10) {
+					beaterCounter = 0;
+					doBeat();
+				}
+			}
+		}
+		
+		beater();
 	}
 	
 	this.draw = function(spriteBatch) {
@@ -26,6 +42,16 @@ function HeartCounter() {
 		for (var i = 0; i < self.totalHeart; i++) {
 			if (i >= self.totalLives) { h = voidHeart; }
 			spriteBatch.drawImage(h, heartSize * i, 100, heartSize, heartSize);
+		}
+	}
+	
+	function doBeat() {
+		beatSize *= -1;
+		heartSize -= beatSize;
+		beatSwitch++;
+		if (beatSwitch >= 4) {
+			beatSwitch = 0;
+			beater = function () {};
 		}
 	}
 }
